aa176eff6855ae6ac76c2768c44ee3282c1c10ed
emily172 <20092335@mail.wit.ie>
2023-02-23 14:29:37 +0000

    Created shoe arraylist updated bug logcat

--- app/src/main/java/ie/setu/shoeapp/activities/ShoeActivity.kt
+++ app/src/main/java/ie/setu/shoeapp/activities/ShoeActivity.kt
@@ -12,32 +12,29 @@ class ShoeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityShoeBinding
     var shoe = ShoeModel()
+    val shoes = ArrayList<ShoeModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityShoeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         Timber.plant(Timber.DebugTree())
-
         i("Shoe Activity started...")
 
         binding.btnAdd.setOnClickListener() {
-            i("add Button Pressed")
-
-            binding.btnAdd.setOnClickListener() {
-                val shoeTitle = binding.shoeTitle.text.toString()
-                if (shoeTitle.isNotEmpty()) {
-                    i("add Button Pressed: $shoeTitle")
-                }
-                else {
-                    Snackbar
-                        .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
-                        .show()
-                }
+            shoe.title = binding.shoeTitle.text.toString()
+            if (shoe.title.isNotEmpty()) {
+                shoes.add(shoe.copy())
+                i("add Button Pressed: ${shoe}")
+                for (i in shoes.indices)
+                { i("Shoe[$i]:${this.shoes[i]}") }
             }
-
+            else {
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
         }
+
     }
 }
+
