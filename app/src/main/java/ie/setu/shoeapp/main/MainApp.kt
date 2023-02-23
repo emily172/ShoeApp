318faf043f7a931d85db74b1971423ee232ee0fa
emily172 <20092335@mail.wit.ie>
2023-02-23 14:51:24 +0000

    Creating MainApp and linking

--- app/src/main/java/ie/setu/shoeapp/main/MainApp.kt
+++ app/src/main/java/ie/setu/shoeapp/main/MainApp.kt
@@ -1,4 +1,14 @@
 package ie.setu.shoeapp.main
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.i
 
-class MainApp {
+
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        i("Shoe started")
+    }
 }
