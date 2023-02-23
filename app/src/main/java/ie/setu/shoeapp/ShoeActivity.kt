ce58c998e7d69ea0bdbae20e1ae7d6274ae894b7
emily172 <20092335@mail.wit.ie>
2023-02-23 13:14:49 +0000

    Updated added Event Handling

--- app/src/main/java/ie/setu/shoeapp/ShoeActivity.kt
+++ app/src/main/java/ie/setu/shoeapp/ShoeActivity.kt
@@ -2,18 +2,40 @@ package ie.setu.shoeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import ie.setu.shoeapp.databinding.ActivityShoeBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
-
 class ShoeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityShoeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shoe)
+
+        binding = ActivityShoeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
+
         i("Shoe Activity started...")
 
+        binding.btnAdd.setOnClickListener() {
+            i("add Button Pressed")
 
+            binding.btnAdd.setOnClickListener() {
+                val placemarkTitle = binding.shoeTitle.text.toString()
+                if (placemarkTitle.isNotEmpty()) {
+                    i("add Button Pressed: $placemarkTitle")
+                }
+                else {
+                    Snackbar
+                        .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                        .show()
+                }
+            }
+
+        }
     }
 }
