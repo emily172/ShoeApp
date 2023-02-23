250724b4bbd5158c2cb8255de5642d2e9dcbcbbf
emily172 <20092335@mail.wit.ie>
2023-02-23 10:02:39 +0000

    Initialise the standard Java Timber by 'planting a tree'

--- app/src/main/java/ie/setu/shoeapp/ShoeActivity.kt
+++ app/src/main/java/ie/setu/shoeapp/ShoeActivity.kt
@@ -2,10 +2,16 @@ package ie.setu.shoeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+
 
 class ShoeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shoe)
+
+        Timber.plant(Timber.DebugTree())
+        Timber.i("Shoe Activity started..")
+
     }
 }
