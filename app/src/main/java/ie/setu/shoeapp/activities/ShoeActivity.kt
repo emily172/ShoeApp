594a9e24c87e660dbc451235f83f96e894535c90
emily172 <20092335@mail.wit.ie>
2023-02-23 15:20:10 +0000

    Tested Null Safety

--- app/src/main/java/ie/setu/shoeapp/activities/ShoeActivity.kt
+++ app/src/main/java/ie/setu/shoeapp/activities/ShoeActivity.kt
@@ -4,38 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import ie.setu.shoeapp.databinding.ActivityShoeBinding
+import ie.setu.shoeapp.main.MainApp
 import ie.setu.shoeapp.models.ShoeModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class ShoeActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityShoeBinding
     var shoe = ShoeModel()
-    val shoes = ArrayList<ShoeModel>()
+
+    var app : MainApp? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShoeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Timber.plant(Timber.DebugTree())
-        i("Shoe Activity started...")
 
+        app = application as MainApp
+        i("Shoe Activity started...")
         binding.btnAdd.setOnClickListener() {
             shoe.title = binding.shoeTitle.text.toString()
             shoe.description = binding.description.text.toString()
             if (shoe.title.isNotEmpty()) {
-                shoes.add(shoe.copy())
+                app!!.shoes.add(shoe.copy())
                 i("add Button Pressed: ${shoe}")
-                for (i in shoes.indices)
-                { i("Shoe[$i]:${this.shoes[i]}") }
+                for (i in app!!.shoes.indices)
+                { i("Shoe[$i]:${this.app!!.shoes[i]}") }
             }
             else {
                 Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
-
     }
 }
 
