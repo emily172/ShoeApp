fe8141d60658e07309b74938a72d549199e55e4f
emily172 <20092335@mail.wit.ie>
2023-02-28 10:19:22 +0000

    Encapsulated the list of shoes

--- app/src/main/java/ie/setu/shoeapp/models/ShoeMemStore.kt
+++ app/src/main/java/ie/setu/shoeapp/models/ShoeMemStore.kt
@@ -1,5 +1,13 @@
 package ie.setu.shoeapp.models
 
+import timber.log.Timber.i
+
+var lastId = 0L
+
+internal fun getId(): Long {
+    return lastId++
+}
+
 class ShoeMemStore : ShoeStore {
 
     val shoes = ArrayList<ShoeModel>()
@@ -8,7 +16,29 @@ class ShoeMemStore : ShoeStore {
         return shoes
     }
 
+
     override fun create(shoe: ShoeModel) {
+        shoe.id = getId()
         shoes.add(shoe)
+        logAll()
+    }
+
+
+
+    override fun update(shoe: ShoeModel) {
+        var foundShoe: ShoeModel? = shoes.find { p -> p.id == shoe.id }
+        if (foundShoe != null) {
+            foundShoe.title = shoe.title
+            foundShoe.description = shoe.description
+            foundShoe.price = shoe.price
+            foundShoe.size = shoe.size
+            foundShoe.shoecolor = shoe.shoecolor
+            logAll()
+        }
+    }
+
+
+    fun logAll() {
+        shoes.forEach{ i("$it") }
     }
 }
